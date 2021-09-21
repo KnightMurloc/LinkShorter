349eec046b804ad6cfc43219d0f48dc3d7d72b63
Murloc Knight <murlockkinght@gmail.com>
2021-09-21 13:20:50 +0700

    init

--- LinkShorter/Controllers/RedirectController.cs
+++ LinkShorter/Controllers/RedirectController.cs
@@ -11,14 +11,12 @@ namespace LinkShorter.Controllers
 	[Controller]
 	public class RedirectController : ControllerBase
 	{
-		//private LinkContext db;
 		private IRepositoryBase<Link> repository;
 		private IConfiguration _configuration;
 		private string addres;
 
 		public RedirectController(IRepositoryBase<Link> repository, IConfiguration configuration)
 		{
-			//this.db = db;
 			this.repository = repository;
 			_configuration = configuration;
 			addres = configuration.GetValue<String>("addres");
@@ -27,17 +25,13 @@ namespace LinkShorter.Controllers
 		[Route("g/{id:int}")]
 		public RedirectResult Get(int id)
 		{
-			Console.WriteLine("test: " + id);
 			Link link = repository.Find(id);
-			//Link link = db.links.Find(id);
-			//Console.WriteLine(link);
 			if (link == null || link.hash == 0)
 			{
 				return Redirect(addres + "/notfound");
 			}
 			else
 			{
-				Console.WriteLine(link.fullLink);
 				return Redirect(link.fullLink);
 			}
 
